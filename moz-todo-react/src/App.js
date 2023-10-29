4c1d91a2d89e2cb21ce9b729ef28a22f3b08c550
Victor Eckert <veckert@uccs.edu>
2023-10-28 21:22:44 -0600

    Completed part 5 of 6 of the React tutorial.

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -4,12 +4,30 @@ import FilterButton from "./components/FilterButton";
 import React, { useState } from "react";
 import { nanoid } from "nanoid"
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
-  const [tasks, setTasks] = useState(props.tasks)
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
   
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return {...task, name: newName};
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
   }
 
   function toggleTaskCompleted(id) {
@@ -27,7 +45,9 @@ function App(props) {
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks.map((task) => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
     <Todo 
       id={task.id} 
       name={task.name} 
@@ -35,6 +55,16 @@ function App(props) {
       key={task.id}
       toggleTaskCompleted={toggleTaskCompleted}
       deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
     />
   ));
 
@@ -46,9 +76,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
